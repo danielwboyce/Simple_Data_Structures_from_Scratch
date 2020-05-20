6d8ec2296c12e2db1c9696cad2b8953064034362
danielwboyce <danielwboyce@gmail.com>
2020-05-20 15:08:24 -0600

    python min-heap and graph

---
+++ Python/graph.py
@@ -0,0 +1,44 @@
+# Uses a dictionary as an adjacency list
+# Connected vertex names are keys and the edge weights are values
+# Has methods to add edges and return a list of connected vertices
+class Vertex:
+  def __init__(self, value):
+    self.value = value
+    self.edges = {}
+
+  def add_edge(self, vertex, weight = 0):
+    self.edges[vertex] = weight
+
+  def get_edges(self):
+    return list(self.edges.keys())
+
+# Can be initialized as a directed graph
+# Stores every vertex inside a dictionary (vertex data is key and vertex is value)
+# has methods to add vertices, edges between vertices, and determine if a path exists between to vertices
+# TODO:Pathfinding does not handle weighted edges yet
+class Graph:
+  def __init__(self, directed = False):
+    self.graph_dict = {}
+    self.directed = directed
+
+  def add_vertex(self, vertex):
+    self.graph_dict[vertex.value] = vertex
+
+  def add_edge(self, from_vertex, to_vertex, weight = 0):
+    self.graph_dict[from_vertex.value].add_edge(to_vertex.value, weight)
+    if not self.directed:
+      self.graph_dict[to_vertex.value].add_edge(from_vertex.value, weight)
+
+  def find_path(self, start_vertex, end_vertex):
+    start = [start_vertex]
+    seen = {}
+    while len(start) > 0:
+      current_vertex = start.pop(0)
+      seen[current_vertex] = True
+      print("Visiting " + current_vertex)
+      if current_vertex == end_vertex:
+        return True
+      else:
+        vertices_to_visit = set(self.graph_dict[current_vertex].edges.keys())
+        start += [vertex for vertex in vertices_to_visit if vertex not in seen]
+    return False
